cb2a643a93158b48c750c543195b48bde0b71234
ray <961882@qq.com>
2017-05-29 20:48:53 +0800

    2017-05-29-04

--- Mayler/spiders/FarmSpider.py
+++ Mayler/spiders/FarmSpider.py
@@ -6,7 +6,8 @@ class FarmSpider(scrapy.spiders.Spider):
     name = "farm"  # 爬虫的名字，执行时使用
     allowed_domains = ["3w3n.com"]  # 允许爬取的域名，非此域名的网页不会爬取
     start_urls = [
-        "http://www.3w3n.com"  # 起始url，此例只爬着一个页面
+        #"http://www.3w3n.com"  # 起始url，此例只爬着一个页面
+        "http://www.3w3n.com/showPriceDefaultList?r=DBF0650FC49BB3EDEA5996A5188167AC"
     ]
 
     def parse(self, response):  # 真正的爬虫方法
